896802439bf84d72b86507353f6810f8d9f554d5
davipraba <prabadavi1@gmail.com>
2026-06-18 07:58:51 +0700

    feat: implement housekeeping module

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,12 +18,12 @@
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ time() }}">
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-[#FAF9F6]">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="bg-[#1A1A1A] shadow-lg">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
